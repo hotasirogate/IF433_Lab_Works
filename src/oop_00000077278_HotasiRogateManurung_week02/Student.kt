aca062e98626f5c87535c1878275cc8a3e991f62
hotasirogate <hotasimanurung28@gmail.com>
2026-02-26 02:53:13 +0700

    week02: add secondary constructor

--- src/oop_00000077278_HotasiRogateManurung_week02/Student.kt
+++ src/oop_00000077278_HotasiRogateManurung_week02/Student.kt
@@ -11,4 +11,8 @@ class Student (
             println("Data mahasiswa $name mungkin akan bermasalah di sistem.")
         }
     }
+
+    constructor(name: String, nim: String) : this(name, nim, "Non-Matriculated") {
+        println("LOG: Menggunakan constructor jalur umum (Tanpa Jurusan).")
+    }
 }

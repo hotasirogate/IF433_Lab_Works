5209d51f0f079cea86af2af25cde42c93a9509a3
hotasirogate <hotasimanurung28@gmail.com>
2026-02-26 15:11:15 +0700

    week03: simulate stack overflow error in setter

--- src/oop_00000077278_HotasiRogateManurung_week03/Employee.kt
+++ src/oop_00000077278_HotasiRogateManurung_week03/Employee.kt
@@ -2,4 +2,9 @@ package oop_00000077278_HotasiRogateManurung_week03
 
 class Employee(val name: String) {
     var salary: Int = 0
+        set(value) {
+            println("Mencoba set gaji ke: $value")
+
+            this.salary = value
+        }
 }

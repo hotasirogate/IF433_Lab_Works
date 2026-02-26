143e9d14b0e908c1d2c252abd526370e6c094d20
hotasirogate <hotasimanurung28@gmail.com>
2026-02-26 15:23:09 +0700

    week03: test all encapsulation logic in main

--- src/oop_00000077278_HotasiRogateManurung_week03/Main.kt
+++ src/oop_00000077278_HotasiRogateManurung_week03/Main.kt
@@ -2,6 +2,12 @@ package oop_00000077278_HotasiRogateManurung_week03
 
 fun main() {
     val e = Employee("Budi")
-    println("--- Test Error")
-    e.salary = 5000
+
+    e.salary = -1000
+    e.salary = 500000
+    println("Gaji: ${e.salary}")
+
+    e.increasePerformance()
+
+    println("Pajak yang harus dibayar: ${e.tax}")
 }

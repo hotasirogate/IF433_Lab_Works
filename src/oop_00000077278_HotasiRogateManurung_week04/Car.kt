72001337bb5903aedaa0bf635e28e5e2b3387bd0
hotasirogate <hotasimanurung28@gmail.com>
2026-02-26 22:49:05 +0700

    week04: create Car class extending Vehicle

---
+++ src/oop_00000077278_HotasiRogateManurung_week04/Car.kt
@@ -0,0 +1,8 @@
+package oop_00000077278_HotasiRogateManurung_week04
+
+open class Car(brand: String, val numberOfDoors: Int) : Vehicle(brand) {
+
+    fun openTrunk() {
+        println("Bagasi mobil $brand dengan $numberOfDoors pintu dibuka.")
+    }
+}

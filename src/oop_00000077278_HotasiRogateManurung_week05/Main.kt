7f8f2feec9d70b4c146f05bd77f3b4796241159e
hotasirogate <hotasimanurung28@gmail.com>
2026-03-04 10:37:27 +0700

    week05: (task 1) test MatHelper overloading in main

--- src/oop_00000077278_HotasiRogateManurung_week05/Main.kt
+++ src/oop_00000077278_HotasiRogateManurung_week05/Main.kt
@@ -7,4 +7,12 @@ fun main() {
     println("Luas Persegi Panjang: " + mathHelper.hitungLuas(5, 3))
     println("Luas Lingkaran: " + mathHelper.hitungLuas(7.0))
 
+    val luasPersegi = mathHelper.hitungLuas(4)
+    val luasPersegiPanjang = mathHelper.hitungLuas(6, 3)
+    val luasLingkaran = mathHelper.hitungLuas(7.0)
+
+    println("Luas Persegi (sisi = 4) = $luasPersegi")
+    println("Luas Persegi Panjang (6 x 3) = $luasPersegiPanjang")
+    println("Luas Lingkaran (r = 7.0) = $luasLingkaran")
+
 }

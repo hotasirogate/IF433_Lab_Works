5aec73ad96f4067372bfa32e25eb65d76e3c44c5
hotasirogate <hotasimanurung28@gmail.com>
2026-03-04 10:33:44 +0700

    week05: (task 1) create MatHelper class with overloading

--- src/oop_00000077278_HotasiRogateManurung_week05/Main.kt
+++ src/oop_00000077278_HotasiRogateManurung_week05/Main.kt
@@ -1,26 +1,10 @@
 package oop_00000077278_HotasiRogateManurung_week05
 
 fun main() {
-    val dosen1 = Dosen("Pak Alex", "0123456")
-    val admin1 = Admin("Bu Siti")
+    val mathHelper = MathHelper()
 
-    val daftarPegawai: List<Pegawai> = listOf(dosen1, admin1)
-
-    println("=== AKTIVITAS PEGAWAI ===")
-    for (pegawai in daftarPegawai) {
-        pegawai.bekerja()
-
-        when (pegawai) {
-            is Dosen -> {
-                println("=> Terdeteksi sebagai Dosen (NIDN: ${pegawai.nidn}")
-                pegawai.mengajar()
-            }
-            is Admin -> {
-                println("=> Terdeteksi sebagai Admin")
-                pegawai.doAdminWork()
-            }
-        }
-        println("-----------------")
-    }
+    println("Luas Persegi: " + mathHelper.hitungLuas(4))
+    println("Luas Persegi Panjang: " + mathHelper.hitungLuas(5, 3))
+    println("Luas Lingkaran: " + mathHelper.hitungLuas(7.0))
 
 }

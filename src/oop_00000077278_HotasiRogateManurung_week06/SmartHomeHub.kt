088986531e0ec395a5be619abe41714e145e8f89
hotasirogate <hotasimanurung28@gmail.com>
2026-03-11 13:16:29 +0700

    week06: (task) create SmartHomeHub logic

---
+++ src/oop_00000077278_HotasiRogateManurung_week06/SmartHomeHub.kt
@@ -0,0 +1,19 @@
+package oop_00000077278_HotasiRogateManurung_week06
+
+class SmartHomeHub {
+
+    val devices = mutableListOf<SmartDevice>()
+
+    fun addDevice(device: SmartDevice) {
+        devices.add(device)
+        println("${device.name} berhasil ditambahkan ke Smart Home Hub.")
+    }
+
+    fun turnOffAllSwitches() {
+        for (device in devices) {
+            if (device is Switchable) {
+                device.turnOff()
+            }
+        }
+    }
+}

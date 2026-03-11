5eb73ea12fdd84c00e54d0d90a25923103174e9e
hotasirogate <hotasimanurung28@gmail.com>
2026-03-11 10:42:20 +0700

    week06: fix diamond problem usin explicit super delegation

--- src/oop_00000077278_HotasiRogateManurung_week06/Smartphone.kt
+++ src/oop_00000077278_HotasiRogateManurung_week06/Smartphone.kt
@@ -1,4 +1,8 @@
 package oop_00000077278_HotasiRogateManurung_week06
 
 class Smartphone : Camera, Phone {
+    override fun turnOn() {
+        super<Camera>.turnOn()
+        super<Phone>.turnOn()
+    }
 }

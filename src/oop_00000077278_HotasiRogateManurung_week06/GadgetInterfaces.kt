4acea0a1a98921610d640efaadb2d18d6148f51a
hotasirogate <hotasimanurung28@gmail.com>
2026-03-11 10:38:51 +0700

    week06: create Camera and Phone interface with default methods

---
+++ src/oop_00000077278_HotasiRogateManurung_week06/GadgetInterfaces.kt
@@ -0,0 +1,11 @@
+package oop_00000077278_HotasiRogateManurung_week06
+
+interface Camera {
+    fun turnOn() {println("Lensa kamera terbuka dan sensor aktif.")
+    }
+}
+
+interface Phone {
+    fun trunOn() {println("Sinyal seluler mencari jaringan.")
+    }
+}

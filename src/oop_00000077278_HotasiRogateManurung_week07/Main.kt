efc74d669808be01d0dae3305c122c0029311e6a
hotasirogate <hotasimanurung28@gmail.com>
2026-04-06 02:15:09 +0700

    week07: tes Singleton and Companion Object in main

---
+++ src/oop_00000077278_HotasiRogateManurung_week07/Main.kt
@@ -0,0 +1,11 @@
+package oop_00000077278_HotasiRogateManurung_week07
+
+fun main() {
+    println("=== TEST SINGLETON ===")
+    println("Status: ${DatabaseManager.connectionStatus}")
+    DatabaseManager.connect()
+
+    println("\n=== TEST COMPANION OBJECT ===")
+    val client = NetworkClient.createClient()
+    client.connect()
+}

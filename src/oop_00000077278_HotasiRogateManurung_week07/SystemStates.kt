e5320e631095419091cf098d278e5cba4568e940
hotasirogate <hotasimanurung28@gmail.com>
2026-04-06 02:30:50 +0700

    week07: create ApiResponse sealed class

--- src/oop_00000077278_HotasiRogateManurung_week07/SystemStates.kt
+++ src/oop_00000077278_HotasiRogateManurung_week07/SystemStates.kt
@@ -1,5 +1,13 @@
 package oop_00000077278_HotasiRogateManurung_week07
 
+import java.util.concurrent.ThreadPoolExecutor
+
 enum class AppState {
     STARTING, RUNNING, STOPPED
+}
+
+sealed class ApiResponse {
+    data class Success(val data: String) : ApiResponse()
+    data class Error(val message: String) : ApiResponse()
+    object Loading : ApiResponse()
 }

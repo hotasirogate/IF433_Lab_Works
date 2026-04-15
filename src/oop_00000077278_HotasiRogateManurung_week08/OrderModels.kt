0bf4c6beb7a18dd98c31cadffdc1c63ae125ae4d
hotasirogate <hotasimanurung28@gmail.com>
2026-04-15 10:37:12 +0700

    week08: implement chained safe calls and elvis

--- src/oop_00000077278_HotasiRogateManurung_week08/OrderModels.kt
+++ src/oop_00000077278_HotasiRogateManurung_week08/OrderModels.kt
@@ -3,4 +3,6 @@ package oop_00000077278_HotasiRogateManurung_week08
 class City(val name: String)
 class Address(val city: City?)
 class DeliveryDetails(val address: Address?)
-class Order(val details: DeliveryDetails?, val totalPrice: Int?)
+class Order(val details: DeliveryDetails?, val totalPrice: Int?) {
+    val deliveryDetails: Any
+}

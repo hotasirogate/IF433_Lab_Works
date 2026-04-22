aad32cbfe61c382078cb5d7efd7d1956af18962f
hotasirogate <hotasimanurung28@gmail.com>
2026-04-22 08:59:48 +0700

    week09: create immutable and mutable list

--- src/oop_00000077278_HotasiRogateManurung_week08/Main.kt
+++ src/oop_00000077278_HotasiRogateManurung_week08/Main.kt
@@ -53,4 +53,10 @@ fun main() {
     } catch (e: IllegalArgumentException) {
         println(e.message)
     }
+
+    println("\n=== TEST JAVA INTEROP ===")
+    val javaResponse = LegacyJavaAPI.fetchServerStatus()
+
+    val statusLength = javaResponse!!.length
+    println("Status dari java: $javaResponse (Length: $statusLength)")
 }

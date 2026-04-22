8c53164ff4a7af1c1612656f0c40dfcf429c5b9a
hotasirogate <hotasimanurung28@gmail.com>
2026-04-22 08:23:40 +0700

    week08: create mixed data collection

--- src/oop_00000077278_HotasiRogateManurung_week08/Main.kt
+++ src/oop_00000077278_HotasiRogateManurung_week08/Main.kt
@@ -6,4 +6,22 @@ fun main() {
 
     val destination = emptyOrder.deliveryDetails?.address?.city?.name ?: "Kota Tidak Diketahui"
     println("Tujuan Pengiriman: $destination")
+
+    val validOrder = Order(null, 250000)
+
+    val receipt = validOrder.totalPrice?.let { price ->
+        val tax = price * 0.11
+        "transaksi valid. harga: Rp$price, pajak: Rp$tax"
+    } ?: "transalsi Invalid: harga belum di-set!"
+
+    println(receipt)
+
+    println("\n=== TEST SAFE CASTING ===")
+    val mixedData: List<Any> = listOf(
+        "Smartphone",
+        1500000,
+        UserProfile("Andi", null),
+        "Laptop",
+        4500000.0
+    )
 }

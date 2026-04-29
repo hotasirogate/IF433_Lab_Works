f96b13d115a4522a23cfdf859a69d61e3d567dc4
hotasirogate <hotasimanurung28@gmail.com>
2026-04-29 10:26:43 +0700

    week10: (task) implement search login in repository

--- src/oop_00000077278_HotasiRogateManurung_week10/WalletModels.kt
+++ src/oop_00000077278_HotasiRogateManurung_week10/WalletModels.kt
@@ -1,10 +1,9 @@
 package oop_00000077278_HotasiRogateManurung_week10
 
-// Data class untuk menyimpan informasi koin
 data class Coin(
-    val name: String,
+    override val name: String,
     val balance: Double
-)
+) : Named
 
 // Data class untuk menyimpan informasi transaksi
 data class Transaction(
--- src/oop_00000077278_HotasiRogateManurung_week10/WalletRepository.kt
+++ src/oop_00000077278_HotasiRogateManurung_week10/WalletRepository.kt
@@ -1,17 +1,26 @@
 package oop_00000077278_HotasiRogateManurung_week10
 
-class WalletRepository<T> {
+interface Named {
+    val name: String
+}
 
-    // Menyimpan semua data generic
+class WalletRepository<T : Any> {
+
     private val items = mutableListOf<T>()
 
-    // Menambahkan item ke repository
     fun add(item: T) {
         items.add(item)
     }
 
-    // Mengambil semua item
     fun getAll(): List<T> {
         return items
     }
+
+    // Fungsi filter hanya untuk item yang punya name
+    fun searchByName(query: String): List<T> {
+        return items.filter {
+            it is Named && it.name.contains(query, ignoreCase = true)
+        }
+    }
 }
+

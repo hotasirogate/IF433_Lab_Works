de3ac9e25bddf3b173ee9ea84f4d63160c87dba1
hotasirogate <hotasimanurung28@gmail.com>
2026-04-29 10:22:31 +0700

    week10: (task) create Coin and Transaction models

---
+++ src/oop_00000077278_HotasiRogateManurung_week10/WalletModels.kt
@@ -0,0 +1,13 @@
+package oop_00000077278_HotasiRogateManurung_week10
+
+// Data class untuk menyimpan informasi koin
+data class Coin(
+    val name: String,
+    val balance: Double
+)
+
+// Data class untuk menyimpan informasi transaksi
+data class Transaction(
+    val id: String,
+    val amount: Double
+)

91cd0d51e460a326896086562cdc7ea2b7c60ff3
hotasirogate <hotasimanurung28@gmail.com>
2026-05-06 10:17:55 +0700

    week11: (task) configure Ezviz camera with also

--- src/oop_00000077278_HotasiRogateManurung_week11/SmartHomeDashboard.kt
+++ src/oop_00000077278_HotasiRogateManurung_week11/SmartHomeDashboard.kt
@@ -13,4 +13,12 @@ fun main() {
         homeDevices.add(it)
     }
 
+    SmartDevice("Ezviz Outdoor", "Camera").apply {
+        isOnline = true
+        powerLoad = 5
+    }.also {
+        println("(LOG) Kamera terhubung")
+        homeDevices.add(it)
+    }
+
 }

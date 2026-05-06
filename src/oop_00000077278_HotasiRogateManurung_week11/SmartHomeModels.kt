ff7ebd62c7acffbba51ca5cbc957490deaa62190
hotasirogate <hotasimanurung28@gmail.com>
2026-05-06 10:14:41 +0700

    week11: (task) create extensions function for SmartDevice

--- src/oop_00000077278_HotasiRogateManurung_week11/SmartHomeModels.kt
+++ src/oop_00000077278_HotasiRogateManurung_week11/SmartHomeModels.kt
@@ -6,3 +6,8 @@ data class SmartDevice(
     var isOnline: Boolean = false,
     var powerLoad: Int = 0
 )
+
+fun SmartDevice.diagnose(): String {
+    val status = if (isOnline) "Online" else "Offline"
+    return "[DIAGNOSTIK] $name | Kategori: $category | Status: $status | Daya: $powerLoad Watt"
+}

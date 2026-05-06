e035e84a73e8900fa8713dba41af49d479869f6c
hotasirogate <hotasimanurung28@gmail.com>
2026-05-06 10:17:09 +0700

    week11: (task) configure Philips WiZ using apply

--- src/oop_00000077278_HotasiRogateManurung_week11/SmartHomeDashboard.kt
+++ src/oop_00000077278_HotasiRogateManurung_week11/SmartHomeDashboard.kt
@@ -3,3 +3,14 @@ package oop_00000077278_HotasiRogateManurung_week11
 fun main() {
     // 1. Inisialisasi Penampung
     val homeDevices = mutableListOf<SmartDevice>()
+
+    SmartDevice("", "").apply {
+        name = "Philips WiZ Living Room"
+        category = "Lighting"
+        isOnline = true
+        powerLoad = 12
+    }.also {
+        homeDevices.add(it)
+    }
+
+}

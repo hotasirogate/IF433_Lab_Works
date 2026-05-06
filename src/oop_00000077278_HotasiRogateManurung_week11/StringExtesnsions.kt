51b706dd3f0427ad660d955939f8ab5165e9c0bf
hotasirogate <hotasimanurung28@gmail.com>
2026-05-06 10:04:32 +0700

    week11: create string extension functions

---
+++ src/oop_00000077278_HotasiRogateManurung_week11/StringExtesnsions.kt
@@ -0,0 +1,9 @@
+package oop_00000077278_HotasiRogateManurung_week11
+
+fun String.addGreeting(): String {
+    return "Hello, $this"
+}
+
+fun String.repeatTimes(n: Int): String {
+    return this.repeat(n)
+}

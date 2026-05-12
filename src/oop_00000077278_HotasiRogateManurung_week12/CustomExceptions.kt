797fb300804aeef8578b2f37837ebb82a30aedef
hotasirogate <hotasimanurung28@gmail.com>
2026-05-13 01:30:48 +0700

    week12: implement throw keyword for validation

--- src/oop_00000077278_HotasiRogateManurung_week12/CustomExceptions.kt
+++ src/oop_00000077278_HotasiRogateManurung_week12/CustomExceptions.kt
@@ -4,3 +4,23 @@ class InsufficientFundsException(
     val amount: Double,
     val balance: Double
 ) : Exception("Attempted $amount, balance $balance")
+
+class BankAccount(var balance: Double) {
+
+    fun withdraw(amount: Double) {
+
+        // cek amount harus positif
+        if (amount <= 0) {
+            throw IllegalArgumentException("Amount must be positive")
+        }
+
+        // cek saldo cukup atau tidak
+        if (amount > balance) {
+            throw InsufficientFundsException(amount, balance)
+        }
+
+        // proses penarikan
+        balance -= amount
+        println("Withdrawal successful. Remaining balance: $balance")
+    }
+}
